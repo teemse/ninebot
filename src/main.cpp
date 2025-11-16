d4130998c2121c5d82d7b5c76623381d133c3dc7
teemse <chernikov.artem2000@gmail.com>
2025-11-16 15:20:25 +0300

    format

--- src/main.cpp
+++ src/main.cpp
@@ -6,100 +6,108 @@ const byte lock[] = {0x5A, 0xA5, 0x02, 0x3D, 0x20, 0x02, 0x70, 0x01, 0x00, 0x2D,
 const byte heartbeat[] = {0x5A, 0xA5, 0x01, 0x3D, 0x20, 0x55, 0x7C, 0x7C, 0x54, 0xFE};
 
 // Настройки кнопки
-const int BUTTON_PIN = 5;  // GPIO5 на ESP8266 d1
-bool isLocked = true;       // Начальное состояние
+const int BUTTON_PIN = 5; // GPIO5 на ESP8266 d1
+bool isLocked = true;     // Начальное состояние
 bool lastButtonState = HIGH;
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50;
 
 // Тайминги
 unsigned long lastHeartbeatTime = 0;
-const unsigned long HEARTBEAT_INTERVAL = 4000;  // 4 секунды
+const unsigned long HEARTBEAT_INTERVAL = 4000; // 4 секунды
 unsigned long lastActionTime = 0;
 
 // Визуальная обратная связь через встроенный LED
-void blinkLED(int times) {
+void blinkLED(int times)
+{
   pinMode(LED_BUILTIN, OUTPUT);
-  for (int i = 0; i < times; i++) {
-    digitalWrite(LED_BUILTIN, LOW);   // LED ON (активный низкий уровень)
+  for (int i = 0; i < times; i++)
+  {
+    digitalWrite(LED_BUILTIN, LOW); // LED ON (активный низкий уровень)
     delay(200);
-    digitalWrite(LED_BUILTIN, HIGH);  // LED OFF
-    if (i < times - 1) delay(150);
+    digitalWrite(LED_BUILTIN, HIGH); // LED OFF
+    if (i < times - 1)
+      delay(150);
   }
 }
 
-
-
-
-
-void sendUnlock() {
+void sendUnlock()
+{
   Serial.write(unlock, sizeof(unlock));
   isLocked = false;
   lastActionTime = millis();
-  blinkLED(2);  // 2 быстрых мигания для разблокировки
+  blinkLED(2); // 2 быстрых мигания для разблокировки
 }
 
-void sendLock() {
+void sendLock()
+{
   Serial.write(lock, sizeof(lock));
   isLocked = true;
   lastActionTime = millis();
-  blinkLED(1);  // 1 длинное мигание для блокировки
+  blinkLED(1); // 1 длинное мигание для блокировки
 }
 
-void toggleLockState() {
-  if (isLocked) {
+void toggleLockState()
+{
+  if (isLocked)
+  {
     sendUnlock();
-  } else {
+  }
+  else
+  {
     sendLock();
   }
 }
 
-void sendHeartbeat() {
+void sendHeartbeat()
+{
   Serial.write(heartbeat, sizeof(heartbeat));
 }
 
-void handleButton() {
+void handleButton()
+{
   int reading = digitalRead(BUTTON_PIN);
-  
+
   // Если состояние кнопки изменилось
-  if (reading != lastButtonState) {
+  if (reading != lastButtonState)
+  {
     lastDebounceTime = millis();
   }
-  
+
   // Если прошло достаточно времени для устранения дребезга
-  if ((millis() - lastDebounceTime) > debounceDelay) {
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
     // Если кнопка нажата (LOW, так как INPUT_PULLUP)
-    if (reading == LOW) {
+    if (reading == LOW)
+    {
       toggleLockState();
-      lastDebounceTime = millis();  // Защита от многократных нажатий
+      lastDebounceTime = millis(); // Защита от многократных нажатий
     }
   }
-  
+
   lastButtonState = reading;
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);  // Кнопка на замыкание на GND
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // Кнопка на замыкание на GND
   delay(1000);
   // Начальная разблокировка при старте
   sendUnlock();
 }
 
-void loop() {
+void loop()
+{
   // Обработка кнопки
   handleButton();
-  
+
   // Отправка heartbeat каждые 4 секунды
-  if (millis() - lastHeartbeatTime >= HEARTBEAT_INTERVAL) {
+  if (millis() - lastHeartbeatTime >= HEARTBEAT_INTERVAL)
+  {
     sendHeartbeat();
     lastHeartbeatTime = millis();
   }
-  
+
   delay(10);
 }
-
-
-
-
-
